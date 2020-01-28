ee9674e7bf1846b21cd8d81691a8e7575b80a9c1
Phil Horrex <phorrex@barrack.com>
2020-01-28 15:50:21 -0500

    Color adjustments

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-'use strict'
+"use strict";
 
-const foregroundColor = '#f8f8f2'
-const backgroundColor = '#282a36'
+const foregroundColor = "#f8f8f2";
+const backgroundColor = "#282a36";
 
-const black           = '#282a36'
-const red             = '#ff5555'
-const green           = '#50fa7b'
-const yellow          = '#f1fa8c'
-const blue            = '#bd93f9'
-const magenta         = '#ff79c6'
-const cyan            = '#8be9fd'
-const white           = '#bfbfbf'
-const brightBlack     = '#44475a'
-const brightRed       = '#ff6e67'
-const brightGreen     = '#5af78e'
-const brightYellow    = '#f4f99d'
-const brightBlue      = '#caa9fa'
-const brightMagenta   = '#ff92d0'
-const brightCyan      = '#9aedfe'
-const brightWhite     = '#f8f8f2'
+const black = "#282a36";
+const red = "#ff5555";
+const green = "#50fa7b";
+const yellow = "#f1fa8c";
+const blue = "#bd93f9";
+const magenta = "#ff79c6";
+const cyan = "#8be9fd";
+const gray = "#bfbfbf";
+
+const brightBlack = "#44475a";
+const brightRed = "#ff6e67";
+const brightGreen = "#5af78e";
+const brightYellow = "#f4f99d";
+const brightBlue = "#caa9fa";
+const brightMagenta = "#ff92d0";
+const brightCyan = "#9aedfe";
+const brightWhite = "#f8f8f2";
 
 exports.decorateConfig = config => {
   return Object.assign({}, config, {
     backgroundColor,
     foregroundColor,
     borderColor: black,
-    cursorColor: brightBlack,
+    cursorColor: brightGreen,
     colors: {
       black,
       red,
@@ -34,7 +35,7 @@ exports.decorateConfig = config => {
       blue,
       magenta,
       cyan,
-      white,
+      gray,
 
       // bright
       brightBlack,
@@ -47,14 +48,13 @@ exports.decorateConfig = config => {
       brightWhite
     },
     css: `
-			${config.css || ''}
-			.tabs_list .tab_tab.tab_active .tab_text  {
-				background: ${backgroundColor};
-			}
-
-			.tab_active:before {
-				border-color: rgb(68, 71, 90);
+			${config.css || ""}
+			
+			.tabs_list .tab_tab.tab_active {
+                background: ${backgroundColor};
+                border-bottom-color: ${brightMagenta} !important;
+				border-bottom-width: 1px;
 			}
 		`
-  })
-}
+  });
+};
