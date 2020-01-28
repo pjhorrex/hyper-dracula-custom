0891c3dbcaddb9cb83a19f0125b77b4834035af9
Phil Horrex <phorrex@barrack.com>
2020-01-28 11:50:35 -0500

    Changed colors to match preferences

--- index.js
+++ index.js
@@ -2,16 +2,23 @@
 
 const foregroundColor = '#f8f8f2'
 const backgroundColor = '#282a36'
-const black = '#44475a'
-const red = '#ff5555'
-const green = '#50fa7b'
-const yellow = '#f1fa8c'
-const blue = '#c0a8fa'
-const magenta = '#ff79c6'
-const cyan = '#8be9fd'
-const gray = '#666666'
-const brightBlack = '#999999'
-const brightWhite = '#ffffff'
+
+const black           = '#282a36'
+const red             = '#ff5555'
+const green           = '#50fa7b'
+const yellow          = '#f1fa8c'
+const blue            = '#bd93f9'
+const magenta         = '#ff79c6'
+const cyan            = '#8be9fd'
+const white           = '#bfbfbf'
+const brightBlack     = '#44475a'
+const brightRed       = '#ff6e67'
+const brightGreen     = '#5af78e'
+const brightYellow    = '#f4f99d'
+const brightBlue      = '#caa9fa'
+const brightMagenta   = '#ff92d0'
+const brightCyan      = '#9aedfe'
+const brightWhite     = '#f8f8f2'
 
 exports.decorateConfig = config => {
   return Object.assign({}, config, {
@@ -27,16 +34,16 @@ exports.decorateConfig = config => {
       blue,
       magenta,
       cyan,
-      gray,
+      white,
 
       // bright
       brightBlack,
-      red,
-      green,
-      yellow,
-      blue,
-      magenta,
-      cyan,
+      brightRed,
+      brightGreen,
+      brightYellow,
+      brightBlue,
+      brightMagenta,
+      brightCyan,
       brightWhite
     },
     css: `
